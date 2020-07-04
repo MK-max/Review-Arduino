0a085430263b7d93b7fc6b748dadd4070118c3c9
orawan onsang <orawan.onsang@mail.kmutt.ac.th>
2020-07-04 15:16:14 +0700

    8. Complete

---
+++ 62120501011-PEXAM-MDT112/NO8/NO8.ino
@@ -0,0 +1,55 @@
+#include "TM1637.h"
+
+const int CLK = 17; 
+const int DIO = 16; 
+
+TM1637 sevenSegment(CLK, DIO); 
+
+#include <Stepper.h> 
+
+#define STEPS_PER_360_DEGREE 2038 
+
+Stepper stepper(STEPS_PER_360_DEGREE, 8, 10, 9, 11); 
+
+void setup(){
+
+  tone(8, 900,600);
+  Serial.begin(9600);
+  Serial.println( "Hello MDT" );
+
+  stepper.setSpeed(16); 
+  Serial.println("Step CW 360  Degrees"); 
+  stepper.step(STEPS_PER_360_DEGREE); 
+  delay(900);
+
+  stepper.setSpeed(16); 
+  Serial.println("Step CCW 360  Degrees"); 
+  stepper.step(-STEPS_PER_360_DEGREE); 
+  delay(800);  
+  sevenSegment.init();
+  sevenSegment.set(7); 
+
+  sevenSegment.displayStr("0000");
+  delay(800);
+
+  sevenSegment.displayStr("");
+  delay(800);
+    
+  delay(800);
+  tone(8, 600, 300);
+
+}
+
+void loop(){
+
+    stepper.setSpeed(8);
+    if (digitalRead(2) == 0)
+    {
+      stepper.step(-STEPS_PER_360_DEGREE/6);
+    }
+    if 
+    {
+      stepper.step(-STEPS_PER_360_DEGREE/-6);
+    }
+    
+}
