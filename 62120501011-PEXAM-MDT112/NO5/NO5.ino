ddeedb03f726ba126ca9d04b009ecde4ccc2bcfa
orawan onsang <orawan.onsang@mail.kmutt.ac.th>
2020-07-04 14:22:41 +0700

    5. Complete

--- 62120501011-PEXAM-MDT112/NO5/NO5.ino
+++ 62120501011-PEXAM-MDT112/NO5/NO5.ino
@@ -5,7 +5,26 @@ const int DIO = 16;
 
 TM1637 sevenSegment(CLK, DIO); 
 
+#include <Stepper.h> 
+
+#define STEPS_PER_360_DEGREE 2038 
+
+Stepper stepper(STEPS_PER_360_DEGREE, 8, 10, 9, 11); 
+
 void setup() {
+  tone(8, 900,600);
+  Serial.begin(9600);
+  Serial.println( "Hello MDT" );
+
+  stepper.setSpeed(16); 
+  Serial.println("Step CW 360  Degrees"); 
+  stepper.step(STEPS_PER_360_DEGREE); 
+  delay(900);
+
+  stepper.setSpeed(16); 
+  Serial.println("Step CCW 360  Degrees"); 
+  stepper.step(-STEPS_PER_360_DEGREE); 
+  delay(800);  
   sevenSegment.init();
   sevenSegment.set(7); 
 
