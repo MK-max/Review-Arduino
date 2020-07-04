4b06fa3e55b0d799a0a4e2e4f041b9e74f8b3b62
orawan onsang <orawan.onsang@mail.kmutt.ac.th>
2020-07-04 14:08:59 +0700

    4. Complete”

--- 62120501011-PEXAM-MDT112/NO4/NO4.ino
+++ 62120501011-PEXAM-MDT112/NO4/NO4.ino
@@ -5,7 +5,14 @@
 Stepper stepper(STEPS_PER_360_DEGREE, 8, 10, 9, 11); 
 
 void setup() {
+  tone(8, 900,600);
   Serial.begin(9600);
+  Serial.println( "Hello MDT" );
+
+  stepper.setSpeed(16); 
+  Serial.println("Step CW 360  Degrees"); 
+  stepper.step(STEPS_PER_360_DEGREE); 
+  delay(900);
 
   stepper.setSpeed(16); 
   Serial.println("Step CCW 360  Degrees"); 
