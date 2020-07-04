bca2255b61780cba70fa8c087171ef1eceb27cf0
orawan onsang <orawan.onsang@mail.kmutt.ac.th>
2020-07-04 14:06:44 +0700

    3. Complete

--- 62120501011-PEXAM-MDT112/NO3/NO3.ino
+++ 62120501011-PEXAM-MDT112/NO3/NO3.ino
@@ -5,6 +5,9 @@
 Stepper stepper(STEPS_PER_360_DEGREE, 8, 10, 9, 11); 
 
 void setup() {
+  tone(8, 900,600);
+  Serial.begin(9600);
+  Serial.println( "Hello MDT" );
   Serial.begin(9600);
 
   stepper.setSpeed(16); 
